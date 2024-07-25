1f73935762e46ed025a92485168ef7af0c7c8ea9
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-07-25 10:11:00 -0300

    add a animação

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,21 @@ const myObserver = new IntersectionObserver( (evento) => {
 })
 
 myObserver.observe(threeSectionEl);
+
+// essa constante pegará todos os elementos que estiverem com a teg data-anime
+const target = document.querySelectorAll('[data-anime]');
+
+// vamos criar uma constante que add a classe animate
+
+const classAnimate = 'animate';
+
+function animateScroll(){
+    // Para saber qual a distância da barra lateral do top usaremos 
+    const windowTop = window.scrollY;
+    
+    // Para fazer a comparação entre o top da pagina e o target que eu quero vamos usar esssa função
+}
+
+window.addEventListener('scroll', () => {
+    animateScroll()
+})
