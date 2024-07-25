3168b3c22750807bb2eb252a6e61b20948a4dd68
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-07-25 11:20:37 -0300

    update no efeito suave

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,9 @@ const myObserver = new IntersectionObserver( (evento) => {
 
 myObserver.observe(threeSectionEl);
 
+
+
+
 // essa constante pegará todos os elementos que estiverem com a teg data-anime
 const target = document.querySelectorAll('[data-anime]');
 
@@ -15,9 +18,15 @@ const classAnimate = 'animate';
 
 function animateScroll(){
     // Para saber qual a distância da barra lateral do top usaremos 
-    const windowTop = window.scrollY;
+    const windowTop = window.pageYOffset + 500;
     
     // Para fazer a comparação entre o top da pagina e o target que eu quero vamos usar esssa função
+    target.forEach((elementos) => {
+        if((windowTop) > elementos.offsetTop){
+            elementos.classList.add(classAnimate);
+        }
+        console.log(elementos.offsetTop);
+    })
 }
 
 window.addEventListener('scroll', () => {
