86ac1f7773bb150b58ed2e1bb8f9b1c72ab9096d
Ismael Nasciemnto <117104139+ismaelvieira12@users.noreply.github.com>
2024-07-28 00:39:10 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -8,25 +8,25 @@
 
 
 // essa constante pegará todos os elementos que estiverem com a teg data-anime
-const target = document.querySelectorAll('[data-anime]');
+// const target = document.querySelectorAll('[data-anime]');
 
-// vamos criar uma constante que add a classe animate
+// // vamos criar uma constante que add a classe animate
 
-const classAnimate = 'animate';
+// const classAnimate = 'animate';
 
-function animateScroll(){
-    // Para saber qual a distância da barra lateral do top usaremos 
-    const windowTop = window.pageYOffset + 500;
+// function animateScroll(){
+//     // Para saber qual a distância da barra lateral do top usaremos 
+//     const windowTop = window.pageYOffset + 500;
     
-    // Para fazer a comparação entre o top da pagina e o target que eu quero vamos usar esssa função
-    target.forEach((elementos) => {
-        if((windowTop) > elementos.offsetTop){
-            elementos.classList.add(classAnimate);
-        }
-        console.log(elementos.offsetTop);
-    })
-}
+//     // Para fazer a comparação entre o top da pagina e o target que eu quero vamos usar esssa função
+//     target.forEach((elementos) => {
+//         if((windowTop) > elementos.offsetTop){
+//             elementos.classList.add(classAnimate);
+//         }
+//         console.log(elementos.offsetTop);
+//     })
+// }
 
-window.addEventListener('scroll', () => {
-    animateScroll();
-})
+// window.addEventListener('scroll', () => {
+//     animateScroll();
+// })
