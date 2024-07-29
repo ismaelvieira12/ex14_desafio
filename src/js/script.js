a3e5171e53fc658d617977acc6010fcaf741f553
Ismael Nasciemnto <117104139+ismaelvieira12@users.noreply.github.com>
2024-07-29 12:44:21 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,14 @@
 
 const myObserver = new IntersectionObserver( (evento) => {
-    console.log(evento);
-})
+    console.log(evento)
+    evento.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    })
+});
+const elementos = document.querySelectorAll('.hidden');
 
-const threeSectionEl = document.querySelectorAll('.hidden');
-threeSectionEl.forEach((element) => {
-    myObserver.observer(element)
-})
-
-
-
-// essa constante pegará todos os elementos que estiverem com a teg data-anime
-// const target = document.querySelectorAll('[data-anime]');
-
-// // vamos criar uma constante que add a classe animate
-
-// const classAnimate = 'animate';
-
-// function animateScroll(){
-//     // Para saber qual a distância da barra lateral do top usaremos 
-//     const windowTop = window.pageYOffset + 500;
-    
-//     // Para fazer a comparação entre o top da pagina e o target que eu quero vamos usar esssa função
-//     target.forEach((elementos) => {
-//         if((windowTop) > elementos.offsetTop){
-//             elementos.classList.add(classAnimate);
-//         }
-//         console.log(elementos.offsetTop);
-//     })
-// }
-
-// window.addEventListener('scroll', () => {
-//     animateScroll();
-// })
+elementos.forEach( (element) => {
+    myObserver.observe(element)
+});
