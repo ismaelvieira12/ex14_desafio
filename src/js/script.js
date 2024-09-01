1a8ea944b7138ed2170bcc2f02090cc063f8d4cd
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-01 10:48:59 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,20 @@ const elementos = document.querySelectorAll('.hidden');
 elementos.forEach( (element) => {
     myObserver.observe(element)
 });
+
+const isidBar = document.querySelector('#isida-bar');
+const menuMobile = document.querySelector('.menu-mobile');
+const hamburger = document.querySelector('#menu-hamburger');
+const inputCheck = document.querySelector('#click');
+menuMobile.addEventListener('click', () => {
+    if(inputCheck.checked){
+        hamburger.classList.add('fa-x');
+        hamburger.classList.remove('fa-bars');
+        isidBar.style.display='block';
+    }
+    else{
+        hamburger.classList.remove('fa-x');
+        hamburger.classList.add('fa-bars');
+        isidBar.style.display='none';
+    }
+})
